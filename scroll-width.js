586fc0f32fd398e7618cc12882dcc203c4e34603
mtalhaamjad <103028212+m-talha-amjad@users.noreply.github.com>
2023-02-08 21:16:50 +0500

    Create scroll-width.js

---
+++ scroll-width.js
@@ -0,0 +1,18 @@
+// create a div with the scroll
+  let div = document.createElement('div');
+  
+  div.style.overflowY = 'scroll';
+  div.style.width = '50px';
+  div.style.height = '50px';
+  
+  // must put it in the document, otherwise sizes will be 0
+  document.body.append(div);
+  let scrollWidth = div.offsetWidth - div.clientWidth;
+  
+  div.remove();
+  document.documentElement.style.setProperty('--scroll-width', scrollWidth + 'px');
+
+// use --scroll-width variable in css
+// body {
+//     padding-right: var(--scroll-width);
+// }
