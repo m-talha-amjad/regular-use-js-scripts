c6b90e8eb821659fdd1c1bca085a21a803975fcb
mtalhaamjad <103028212+m-talha-amjad@users.noreply.github.com>
2023-04-01 02:12:56 +0500

    Create shopify-thene-editor-rezize.js

---
+++ shopify-thene-editor-rezize.js
@@ -0,0 +1,9 @@
+$(document).on('themeEditorResize', function(event, mode) {
+  if (mode === 'mobile') {
+    // Do something when the customizer is in mobile view mode.
+  } else if (mode === 'desktop') {
+    // Do something when the customizer is in desktop view mode.
+  } else if (mode === 'fullscreen') {
+    // Do something when the customizer is in full-screen view mode.
+  }
+});
