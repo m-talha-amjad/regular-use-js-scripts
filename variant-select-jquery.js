7099e2e8e557da809b9a86fae552f5e1c6cdbeb2
mtalhaamjad <103028212+m-talha-amjad@users.noreply.github.com>
2023-06-19 17:08:21 +0500

    Update variant-select-jquery.js

--- variant-select-jquery.js
+++ variant-select-jquery.js
@@ -1,8 +1,6 @@
 
                         $(document).ready(function() {
                               let vId = $('.original-selector :selected').val();
-                                let preorder_text = $('.preorder-hidden #preorder-'+vId+'').html();
-                                $("#preorder-avail").html(preorder_text);
                         });
                         $(".option-selector-fieldset").on( "click", ".opt-btn", function() {
                             setTimeout(function() {
@@ -15,14 +13,6 @@
                               else {
                                 last_part = $('.original-selector :selected').val();
                               }
-                              let preorder_text = $('.preorder-hidden #preorder-'+last_part+'').html();
-                              $("#preorder-avail").html(preorder_text);
-                              $('.pre-active').parent('#preorder-avail').next().text('Add to Cart');
                           }, 500);
-                        });
-                        $(document).ready(function(){
-                          setTimeout(function() {
-                            $('.pre-active').parent('#preorder-avail').next().text('Add to Cart');
-                          }, 2000);
                         });
   
