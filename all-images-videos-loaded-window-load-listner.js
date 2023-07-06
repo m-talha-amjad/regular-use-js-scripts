33e02fd7f94bd7e80b6a1489a7d277fa86ad332e
mtalhaamjad <103028212+m-talha-amjad@users.noreply.github.com>
2023-07-07 02:45:23 +0500

    Update all-images-videos-loaded-window-load-listner.js

--- all-images-videos-loaded-window-load-listner.js
+++ all-images-videos-loaded-window-load-listner.js
@@ -1,8 +1,8 @@
 /* Desktop and mobile posters */
 
   window.addEventListener("load", (event) => {
-    let vidPosterDesk = document.querySelector("#section-{{ section.id }} .vid_poster_desk");
-    let vidPosterMobile = document.querySelector("#section-{{ section.id }} .vid_poster_mobile");
+    let vidPosterDesk = document.querySelector("#shopify-section-{{ section.id }} .vid_poster_desk");
+    let vidPosterMobile = document.querySelector("#shopify-section-{{ section.id }} .vid_poster_mobile");
     if (vidPosterDesk) {
       vidPosterDesk.classList.add("hideIt");
     }
