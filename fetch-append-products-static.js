df34332fc42da8fc3ff8d0b78cb898ffe52c11a1
mtalhaamjad <103028212+m-talha-amjad@users.noreply.github.com>
2023-09-22 00:34:09 +0500

    Create fetch-append-products-static.js
    this can be used for testing in console that your fetching and appending will work.

---
+++ fetch-append-products-static.js
@@ -0,0 +1,33 @@
+const loadMoreButton = document.getElementById('view-more-button');
+  try {
+      // Store the URL for the next page
+      const nextUrl = '/collections/all?page=2';
+      loadMoreButton.dataset.pageUrl = nextUrl;
+
+      // Fetch the next page in the background
+      const response = await fetch(nextUrl);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const responseHTML = await response.text();
+      const responseDoc = new DOMParser().parseFromString(responseHTML, 'text/html');
+
+      // Find the product grid in the response
+      const responseGrid = responseDoc.getElementById('product-grid');
+
+      // Append the products from the next page to the existing grid
+      if (responseGrid) {
+        const grid = document.getElementById('product-grid');
+        const newProducts = responseGrid.querySelectorAll('.product-item');
+        newProducts.forEach(function (product) {
+          grid.appendChild(product);
+          product.style.opacity = 1;
+        });
+
+        // Update the URL for the next page of products
+        nextPageUrl = responseGrid.getAttribute('data-next-page-url');
+      }
+    } catch (error) {
+      console.error('Error fetching and appending products:', error);
+    }
