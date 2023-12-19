5a97820de7157ca9421e4d432219dd71a4486efe
mtalhaamjad <103028212+m-talha-amjad@users.noreply.github.com>
2023-12-19 05:27:56 +0500

    Update close-other-opened-accordions.js

--- close-other-opened-accordions.js
+++ close-other-opened-accordions.js
@@ -15,3 +15,21 @@
     }
   });
 });
+
+
+// if siblings are the detail element use this 
+
+ document.querySelectorAll('.collection-sidebar__wrapper .collection-sidebar__filter-group summary').forEach((summary) => {
+    summary.addEventListener('click', (event) => {
+        const currentAccordion = event.currentTarget.closest('.collection-sidebar__filter-group');
+        if (currentAccordion) {
+        document.querySelectorAll('.collection-sidebar__filter-group').forEach((accordion) => {
+            if (accordion !== currentAccordion) {
+            // Close other accordions
+            accordion.removeAttribute('open');
+            // accordion.querySelector('summary').setAttribute('aria-expanded', 'false');
+            }
+        });
+        }
+    });
+    });
