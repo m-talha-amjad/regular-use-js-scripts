1002b9db2f4b0001e5c71d293e024de9127ac477
mtalhaamjad <103028212+m-talha-amjad@users.noreply.github.com>
2024-04-23 04:02:37 +0500

    Update set-max-height.js

--- set-max-height.js
+++ set-max-height.js
@@ -1,8 +1,29 @@
-	var elements = document.querySelectorAll(".grid-item .includes");
-	var max_height = 0;
-	for (var i = 0; i < elements.length; i++) {
-		max_height = Math.max(max_height, elements[i].offsetHeight);
-	}
-	for (var i = 0; i < elements.length; i++) {
-  elements[i].style.height = max_height + "px";
-}
+//  .grid__item .card>.card__content .card__heading {
+//   min-height: var(--pi-max-height-title);
+// }
+// .grid__item .card>.card__content .card__information {
+//   min-height: var(--pi-max-height-card__information);
+// }
+
+
+ var facetsWrapper = document.querySelector("facet-filters-form.facets+.facets-vertical");
+
+  var card__heading = document.querySelectorAll(".grid__item .card>.card__content .card__heading");
+
+  var card__heading_max_height = 0;
+
+  for (var i = 0; i < card__heading.length; i++) {
+    card__heading_max_height = Math.max(card__heading_max_height, card__heading[i].offsetHeight);
+  }
+
+  facetsWrapper.style.setProperty("--pi-max-height-title", card__heading_max_height + "px");
+
+
+  var card__information = document.querySelectorAll(".grid__item .card>.card__content .card__information");
+
+  var card__information_max_height = 0;
+
+  for (var i = 0; i < card__information.length; i++) {
+    card__information_max_height = Math.max(card__information_max_height, card__information[i].offsetHeight);
+  }
+  facetsWrapper.style.setProperty("--pi-max-height-card__information", card__information_max_height + "px");
